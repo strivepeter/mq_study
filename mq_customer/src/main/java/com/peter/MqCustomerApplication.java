33bef188324301897895a778a9e15f82512765f9
zhangheng <zhangheng@moguyun.com>
2019-07-09 19:43:24 +0800

    提交代碼

---
+++ mq_customer/src/main/java/com/peter/MqCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.peter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqCustomerApplication.class, args);
+	}
+
+}
---
+++ mq_provide/src/main/java/com/peter/MqProvideApplication.java
@@ -0,0 +1,13 @@
+package com.peter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqProvideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqProvideApplication.class, args);
+	}
+
+}
